2da95f7f29acb071cf3177077b55d2dd2642d474
stanley-ryan <stanley_81724@hotmail.com>
2019-04-14 22:25:23 +0800

    update input

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ val_gf_file_path='/media/fish/DATA/data/ILSVRC2012/ground_truth/val_annotation_f
 
 IM_H=224
 IM_W=224
-TRAIN_BATCH_SIZE=1
+TRAIN_BATCH_SIZE=2
 def gen_train_dataset():
 	f=open(train_gf_file_path)
 	for line in f.readlines():
@@ -14,16 +14,34 @@ def gen_train_dataset():
 		label=int(lines[1])
 		im=cv2.imread(lines[0])
 		im=cv2.resize(im,(IM_W,IM_H))
-		yield (im,label)
+		im=im.astype('uint8')
+		feature_data=np.zeros([IM_H,IM_W,3])
+		feature_data=im
+		yield (feature_data,label)
 
 
 def get_train_dataset():
-	dataset=tf.data.Dataset.from_generator(gen_train_dataset,(tf.float32,tf.int32),(tf.TensorShape([IM_H,IM_W,3]),tf.TensorShape([])))
-	dataset=dataset.batch(TRAIN_BATCH_SIZE)
+	dataset=tf.data.Dataset.from_generator(gen_train_dataset,(tf.uint8,tf.int32),(tf.TensorShape([IM_H,IM_W,3]),tf.TensorShape([])))
+	dataset=dataset.shuffle(1000).batch(TRAIN_BATCH_SIZE).repeat(10)
 	return dataset
 
 if __name__ == '__main__':
 	dataset=get_train_dataset()
+	iterator = dataset.make_initializable_iterator()
+	features_elem,label_elem = iterator.get_next()
+	print("ITERATOR",iterator)
+	print("FEATURES",features_elem)
+	print("LABELS",label_elem)
+
+	features_placeholder = tf.placeholder(features_elem.dtype, features_elem.shape)
+	labels_placeholder = tf.placeholder(label_elem.dtype, label_elem.shape)
+
+	sess=tf.Session()
+	sess.run(iterator.initializer)
+	feature_data=sess.run(features_elem)
+	label_data=sess.run(label_elem)
+	sess.run(iterator.initializer,feed_dict={features_placeholder: feature_data,labels_placeholder: label_data})
+
 '''
 	iterr=dataset.make_one_shot_iterator()
 	im_data,label_data=iterr.get_next()
